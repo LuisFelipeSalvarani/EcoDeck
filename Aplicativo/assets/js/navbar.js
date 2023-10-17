caaa971a5e34afbc0e330b596977adc168484e40
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2023-10-16 23:54:57 -0300

    header e menu mobile funcionando

--- Aplicativo/assets/js/navbar.js
+++ Aplicativo/assets/js/navbar.js
@@ -1,35 +1,10 @@
-// const btnMobile = document.getElementById('hamburguer');
+const hamburguer = document.getElementById('hamburguer');
+const menuLinks = document.getElementById('menu-links');
+const links = document.querySelectorAll('.menu-links li');
 
-// function toggleMenu(event) {
-//     if(event.type === 'touchstart') event.preventDefault();
-//     const nav = document.getElementById('menu');
-//     nav.classList.toggle('active');
-//     const active = nav.classList.contains('active');
-//     event.currentTarget.setAttribute('aria-expanded', active);
-//     if(active) {
-//         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-//     } else {
-//         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-//     }
-
-//     btnMobile.addEventListener('click', toggleMenu);
-//     btnMobile.addEventListener('touchstart', toggleMenu);
-// }
-
-const btnMobile = document.getElementById('hamburguer');
-const nav = document.getElementById('menu');
-
-function toggleMenu(event) {
-    if (event.type === 'touchstart') {
-        event.preventDefault();
-    }
-    
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    
-    btnMobile.setAttribute('aria-expanded', active);
-    btnMobile.setAttribute('aria-label', active ? 'Fechar Menu' : 'Abrir Menu');
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+hamburguer.addEventListener("click", () => {
+    menuLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
